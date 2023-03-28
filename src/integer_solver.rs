542e8cdb0bc634d714a62c119417ec4c3118945b
xRetry <marcoschwarz.at@gmail.com>
2023-03-28 18:36:27 +0200

    Add more solver tests

--- src/integer_solver.rs
+++ src/integer_solver.rs
@@ -91,7 +91,7 @@ impl SolverModel for IntegerSolver {
 
 #[cfg(test)]
 mod tests {
-    use good_lp::{ProblemVariables, variable, constraint, SolverModel};
+    use good_lp::{ProblemVariables, variable, constraint, SolverModel, Solution, Variable, Expression};
 
     use super::IntegerSolver;
 
@@ -108,5 +108,61 @@ mod tests {
         assert!(solver.int_vars.len() == 1 && solver.int_vars.contains(&a));
         assert!(solver.problem.variables.iter_variables_with_def()
             .filter(|(_, v)| v.is_integer).count() == 0)
+    }
+
+    #[test]
+    fn test_example1() {
+        let mut problem = ProblemVariables::new();
+        let x1 = problem.add(variable().integer().min(0));
+        let x2 = problem.add(variable().integer().min(0));
+        let solution = problem.maximise(5*x1 + 8*x2).using(IntegerSolver::new)
+            .with(constraint!(x1 + x2 <= 6))
+            .with(constraint!(5*x1 + 9*x2 <= 45))
+            .solve().unwrap();
+
+        assert!(solution.value(x1) - 0. < 10e-6);
+        assert!(solution.value(x1) - 5. < 10e-6);
+    }
+
+    #[test]
+    fn test_example2() {
+        let mut problem = ProblemVariables::new();
+        let c = problem.add(variable().integer().min(0));
+        let t = problem.add(variable().integer().min(0));
+        let solution = problem.maximise(12*c + 13*t).using(IntegerSolver::new)
+            .with(constraint!(6*c + 7*t <= 21))
+            .solve().unwrap();
+
+        assert!(solution.value(c) - 0. < 10e-6);
+        assert!(solution.value(t) - 3. < 10e-6);
+    }
+
+    #[test]
+    fn test_example3() {
+        let weights_obj = [0.1, 0.04, 0.07, 0.12, 0.08, 0.03, 0.04, 0.05];
+        let weights_constr = [9., 7., 5., 8., 5., 3., 7., 10.];
+
+        let mut problem = ProblemVariables::new();
+        let vars: Vec<Variable> = (0..weights_obj.len())
+            .map(|_| problem.add(variable().integer().min(0).max(1)))
+            .collect();
+
+        let obj: Expression = weights_obj.iter()
+            .zip(&vars)
+            .map(|(w, v)| *w * *v)
+            .sum();
+
+        let constr_lhs: Expression = weights_constr.iter()
+            .zip(&vars)
+            .map(|(w, v)| *w * *v)
+            .sum();
+
+        let solution = problem.maximise(obj).using(IntegerSolver::new)
+            .with(constraint!(constr_lhs <= 40))
+            .solve().unwrap();
+
+        for i in 0..vars.len() {
+            assert!(solution.value(vars[i]) - ((i != 6) as i32 as f64) < 10e-6);
+        }
     }
 }
