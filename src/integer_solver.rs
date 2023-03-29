3da65f58540d664bc8046457f6a9737a9b959a8d
xRetry <marcoschwarz.at@gmail.com>
2023-03-29 09:34:03 +0200

    Add minimization

--- src/integer_solver.rs
+++ src/integer_solver.rs
@@ -1,7 +1,8 @@
 use std::collections::HashSet;
 use good_lp::{constraint, solvers::highs::{highs, HighsSolution}, 
     variable::UnsolvedProblem, SolverModel, 
-    Solution, Constraint, Variable, ResolutionError
+    Solution, Constraint, Variable, ResolutionError,
+    solvers::ObjectiveDirection,
 };
 
 pub struct IntegerSolver {
@@ -9,6 +10,7 @@ pub struct IntegerSolver {
     constraints: Vec<Constraint>,
     int_vars: HashSet<Variable>,
     best_solution: Option<HighsSolution>,
+    fn_is_better: fn(f64, f64) -> bool,
 }
 
 impl IntegerSolver {
@@ -22,12 +24,18 @@ impl IntegerSolver {
         for v in problem.variables.variables.iter_mut() {
             v.is_integer = false;
         }
-        
+
+        let fn_is_better = match problem.direction {
+            ObjectiveDirection::Maximisation => |cur, best| cur > best,
+            ObjectiveDirection::Minimisation => |cur, best| cur < best,
+        };
+
         IntegerSolver { 
             problem,
             constraints: Vec::new(),
             int_vars,
             best_solution: None,
+            fn_is_better,
         }
     }
 
@@ -53,7 +61,7 @@ impl IntegerSolver {
         
         // Current solution worse than best -> stop
         if let Some(best_solution) = &self.best_solution {
-            if objective_value < objective.eval_with(best_solution) { return; }
+            if !(self.fn_is_better)(objective_value, objective.eval_with(best_solution)) { return; }
         }
 
         // At this point, the current solution is better
@@ -111,6 +119,26 @@ mod tests {
     }
 
     #[test]
+    fn test_minimization() {
+        let mut problem = ProblemVariables::new();
+        let x = problem.add(variable().integer().min(5.5));
+        let solution = problem.minimise(x).using(IntegerSolver::new)
+            .solve().unwrap();
+
+        assert!(solution.value(x) - 6. < 10e-6);
+    }
+
+    #[test]
+    fn test_maximization() {
+        let mut problem = ProblemVariables::new();
+        let x = problem.add(variable().integer().max(5.5));
+        let solution = problem.maximise(x).using(IntegerSolver::new)
+            .solve().unwrap();
+
+        assert!(solution.value(x) - 5. < 10e-6);
+    }
+
+    #[test]
     fn test_example1() {
         let mut problem = ProblemVariables::new();
         let x1 = problem.add(variable().integer().min(0));
@@ -164,5 +192,71 @@ mod tests {
         for i in 0..vars.len() {
             assert!(solution.value(vars[i]) - ((i != 6) as i32 as f64) < 10e-6);
         }
+    }
+
+    #[test]
+    fn test_example_abs() {
+        let mut problem = ProblemVariables::new();
+        let x = problem.add(variable().max(2));
+        let t = problem.add(variable());
+        let solution = problem.minimise(t).using(IntegerSolver::new)
+            .with(constraint!(x-3 <= t))
+            .with(constraint!(-(x-3) <= t))
+            .solve().unwrap();
+
+        assert!(solution.value(x) - 2. < 10e-6);
+    }
+
+    #[test]
+    fn test_example_equal_split() {
+        let mut problem = ProblemVariables::new();
+        let x1 = problem.add(variable().integer().min(0).max(1));
+        let x2 = problem.add(variable().integer().min(0).max(1));
+        let t = problem.add(variable());
+        let solution = problem.minimise(t).using(IntegerSolver::new)
+            .with(constraint!((2*x1-1) + (2*x2-1) <= t))
+            .with(constraint!(-((2*x1-1) + (2*x2-1)) <= t))
+            .solve().unwrap();
+
+        assert!(
+            (solution.value(x1) - 1. < 10e-6
+            && solution.value(x2) - 0. < 10e-6)
+            || (solution.value(x1) - 0. < 10e-6
+            && solution.value(x2) - 1. < 10e-6)
+        );
+    }
+
+    #[test]
+    fn test_example_equal_split2() {
+        let weights_obj = [1., 0.4, 0.4];
+
+        let mut problem = ProblemVariables::new();
+
+        let vars: Vec<Variable> = (0..weights_obj.len())
+            .map(|_| problem.add(variable().integer().min(0).max(1)))
+            .collect();
+        let diff = problem.add(variable());
+
+        let constr_weights: Expression = weights_obj.iter()
+            .zip(&vars)
+            .map(|(w, v)| (2 * *v - 1) * *w)
+            .sum();
+
+        let solution = problem.minimise(diff).using(IntegerSolver::new)
+            .with(constraint!(constr_weights.clone() <= diff))
+            .with(constraint!(-constr_weights <= diff))
+            .solve().unwrap();
+
+        assert!(
+            vars.len() == vars.iter().enumerate()
+                .map(|(i, v)| solution.value(*v) - ((i == 0) as i32 as f64))
+                .map(|d| (d < 10e-6) as usize)
+                .sum()
+            ||
+            vars.len() == vars.iter().enumerate()
+                .map(|(i, v)| solution.value(*v) - ((i != 0) as i32 as f64))
+                .map(|d| (d < 10e-6) as usize)
+                .sum()
+        );
     }
 }
